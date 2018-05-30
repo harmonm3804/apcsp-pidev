a3d4bf5514a6835b020af8425c69da9e65761bdf
Megan Harmon <megan.lrh@gmail.com>
2018-05-30 21:55:36 +0000

    first

---
+++ cdemo/userInput.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+float areaCircle(float r)
+{
+float a = 3.14*r*r;
+}
+
+float main ()
+{
+  char input[256];
+
+  printf("What is the desired radius range?\n");
+  fgets(input, 256, stdin);
+
+  float r;
+  if (sscanf(input, "%f", &num) == 1);
+
+  float a;
+
+  for (float r = ; r < ; r++)
+{
+  a = areaCircle (r);
+
+ printf("%f\n", a);}
+
+
+}
+
