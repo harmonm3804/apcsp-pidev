a28589d410b6bb2f001ffb87adf828c27e1002bc
Megan Harmon <megan.lrh@gmail.com>
2018-05-31 21:04:20 +0000

    first

--- cdemo/arrayAdd.c
+++ cdemo/arrayAdd.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
-int arr [100]
+void arrayAdd(int arr[], int s, int n)
 
-for (int i = 0; i < = 100; i++)
+{
+
+ for (int i = 0; i < s; i++)
+
+ {
+  arr[i]=arr[i]+n;
+  printf("%d\n",  arr[i]);
+ }
+}
+
+int main()
+
+{
+int arr[100];
+for (int i = 0; i < 100; i++)
   {
     arr [i] = i * i;
   }
+ arrayAdd(arr, 100, 5);
+}
