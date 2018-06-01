7cd3cda79c5f27c4528d09c88b0ddaf64d721772
Megan Harmon <megan.lrh@gmail.com>
2018-06-01 21:46:33 +0000

    first

--- cdemo/userInput.c
+++ cdemo/userInput.c
@@ -3,28 +3,60 @@
 float areaCircle(float r)
 {
 float a = 3.14*r*r;
+return a;
 }
+
+float radiustop()
+  {
+
+    float rtop;
+    char input2 [256];
+    int correct2 = 0;
+
+    while (correct2 == 0)
+      {
+      printf("What is the top radius for your desired range?\n");
+      fgets (input2, 256, stdin);
+      correct2 = sscanf (input2, "%f", &rtop);
+      }
+    if (correct2 != 1)
+     {
+     printf("Enter a legitimate value. :( \n");
+     }
+  }
+
+
+float bottomradius()
+ {
+  float bottomradius;
+  char input [256];
+  int correct = 0;
+
+  while (correct == 0)
+
+  {
+      printf("What is the bottom radius for your desired range?\n");
+      fgets (input, 256, stdin);
+      correct = sscanf (input, "%f", &bottomradius);
+  }
+  if (correct != 1)
+    {
+     printf("Enter a legitimate value :( \n");
+    }
+  return bottomradius;
+ }
 
 float main ()
-{
-  char input[256];
-
-  printf("What is the desired radius range?\n");
-  fgets(input, 256, stdin);
-
-  float num
-  float r;
-  if (sscanf(input, "%f", &num) == 1);
-
-  float a;
-
-  for (float r = num ; r <  num; r++)
 
 {
-  a = areaCircle (r);
-
- printf("%f\n", a);}
-
-
+   float r;
+   float rtop = radiustop();
+   float rbot = bottomradius();
+{
+for (float r = rbot; r <= rtop; r++);
+  {
+     float a = areaCircle(r);
+     printf("%f\n", a);
+  }
 }
-
+}
